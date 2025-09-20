cbf399381bb3544cfcc000541eba5d40888edac8
dev-kvt <devkvt@proton.me>
2025-09-20 11:21:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import  express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from "url";
+import indexRoutes from "./routes/index.js";
+import soilRoutes from "./routes/soil.js";
+//.env
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+//ejs setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//middlewares 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+//error handlers
+app.use((err, req, res, next) => {
+console.error(err);
+res.status(500).render("error", { message: "Internal Server Error" });
+});
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
